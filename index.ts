f4a88d47834265e1641b0f88636f2e3ad989909a
Shoe Gazer <shoegazer1969@outlook.com>
2024-05-25 10:49:52 -0700

    content type and tweaks

--- index.ts
+++ index.ts
@@ -19,21 +19,26 @@ const tbtcVaultContract = new ethers.Contract(
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/minterBalances", async (req: Request, res: Response) => {
+app.get("/getMinters", async (req: Request, res: Response) => {
+  console.log(`[server] +getMinters`);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const minters = await (tbtcVaultContract as any).getMinters();
 
-  type minterBalance = {
+  type minterInfo = {
     address: string;
     balance: string;
   };
-  const minterBalances = new Array<minterBalance>();
+  const minterInfos = new Array<minterInfo>();
+  const response = { minters: minterInfos };
   for (const minter of minters) {
     const balance = ethers.formatEther(await provider.getBalance(minter));
-    minterBalances.push({ address: minter, balance: balance });
+    minterInfos.push({ address: minter, balance: balance });
   }
 
-  res.send(JSON.stringify(minterBalances));
+  const stringifiedResponse = JSON.stringify(response);
+  res.setHeader("Content-Type", "application/json");
+  console.log(`\treturning ${stringifiedResponse}`);
+  res.send(stringifiedResponse);
 });
 
 app.listen(port, () => {
